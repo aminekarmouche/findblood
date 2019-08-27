f6af676d778ad284f034a2d81dbf55ab25f9a6f4
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2019-08-27 12:02:11 +0000

    signup for owners and donors

--- app.js
+++ app.js
@@ -3,20 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose= require('mongoose');
+//var flash = require('connect-flash');
+var passport = require('passport');
+
+
 
 
 var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/signup');
 var pricesRouter = require('./routes/prices')
 var centersRouter = require('./routes/centers');
+var signupRouter = require('./routes/signup');
 
 var app = express();
+//connect to database
+var mongoDB =  'mongodb+srv://Mehdi:DatabaseMehdy!123@cluster0-lnhl4.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+  .then(() => console.log("MongoDb connected"))
+  .catch(err => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//app.use(flash());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,9 +38,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/prices', pricesRouter);
 app.use('/centers', centersRouter);
+app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 
--- models/donor.js
+++ models/donor.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
-var DonerSchema = new Schema({
+var DonorSchema = new Schema({
     firstName : {type : String , required : true , max:100},
     lastName : { type : String , required : true , max :100},
     userName : { type : String , required : true , max :100},
     email : { type : String , required : true , max :100},
     password: { type : String , required : true , max :100},
-    bloodType : { type : String , required : false , max :100}
+    bloodType: { type : String , required : true , max :100}
 });
 
-module.exports=mongoose.model('Doner', DonerSchema);
+module.exports=mongoose.model('Donor', DonorSchema);
